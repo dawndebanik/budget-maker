b04bb67908fe37f9c58ae2b1c9074fb054930902
debanik.dawn <debanik.dawn@flipkart.com>
2022-11-16 23:55:00 +0530

    refactor

--- src/main/kotlin/SplitwiseClient.kt
+++ src/main/kotlin/SplitwiseClient.kt
@@ -6,16 +6,26 @@ import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.util.EntityUtils
 
+private const val SPLITWISE_V3_BASE_URL = "https://secure.splitwise.com/api/v3.0"
+
 class SplitwiseClient(private var httpClient: HttpClient, private var apiKey: String) {
 
     private val objectMapper = jacksonObjectMapper()
 
     fun getExpenses(): List<Expense> {
-        val request = HttpGet("https://secure.splitwise.com/api/v3.0/get_expenses")
-        request.addHeader("Authorization", "Bearer $apiKey")
-        val response = EntityUtils.toString(httpClient.execute(request).entity)
+        val response = performGetCall("get_expenses")
         val getExpensesResponse: GetExpensesResponse = objectMapper.readValue(response)
 
         return getExpensesResponse.expenses
     }
+
+    private fun performGetCall(endpoint: String): String {
+        val request = HttpGet("$SPLITWISE_V3_BASE_URL/$endpoint").addAuthHeader(apiKey)
+        return EntityUtils.toString(httpClient.execute(request).entity)
+    }
+}
+
+private fun HttpGet.addAuthHeader(apiKey: String): HttpGet {
+    this.addHeader("Authorization", "Bearer $apiKey")
+    return this
 }
