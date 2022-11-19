47ee313951b7ea591bc5d12a5df84b5834698e5d
debanik.dawn <debanik.dawn@flipkart.com>
2022-11-19 18:30:08 +0530

    add more utils

--- src/main/kotlin/utils/ExpenseUtils.kt
+++ src/main/kotlin/utils/ExpenseUtils.kt
@@ -1,6 +1,7 @@
 package utils
 
 import models.domain.Expense
+import models.domain.User
 
 class ExpenseUtils {
     companion object {
@@ -15,6 +16,17 @@ class ExpenseUtils {
                 expense.users?.any {
                     it.userId.equals(userId)
                 } == true
+            }
+        }
+
+        fun getUserFromExpense(userId: String, expense: Expense): User? {
+            return expense.users?.first { it.userId.equals(userId) }
+        }
+
+        fun getUsersShareFromExpenses(userId: String, expenses: List<Expense>): Map<Expense, Double?> {
+            val expensesForUser = getExpensesForUserInvolved(userId, expenses)
+            return expensesForUser.associateWith {
+                getUserFromExpense(userId, it)?.owedShare
             }
         }
     }
