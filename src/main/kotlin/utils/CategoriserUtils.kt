3472ed80f500018d6f70935fe58a5e5be7355c63
debanik.dawn <debanik.dawn@flipkart.com>
2022-11-27 13:41:13 +0530

    add basic categoriser

---
+++ src/main/kotlin/utils/CategoriserUtils.kt
@@ -0,0 +1,35 @@
+package utils
+
+import core.models.domain.Expense
+import java.util.regex.Pattern
+
+class CategoriserUtils {
+    companion object {
+        fun categorise(expenses: List<Expense>, categoryToTokenSetMap: Map<String, Set<String>>): Map<Expense, String?> {
+            return expenses.associateWith { expense -> categorise(expense, categoryToTokenSetMap) }
+        }
+
+        private fun categorise(expense: Expense, categoryToTokenSetMap: Map<String, Set<String>>): String? {
+            val tokenToCategoryMap = reverseMap(categoryToTokenSetMap).mapKeys { mapEntry -> mapEntry.key.lowercase() }
+            val descriptionTokens = tokenize(expense.description)
+            descriptionTokens?.forEach { token ->
+                if (tokenToCategoryMap.containsKey(token)) {
+                    return tokenToCategoryMap[token]
+                }
+            }
+            return expense.category?.name
+        }
+
+        private fun tokenize(description: String?): List<String>? {
+            return description?.trim()?.split(Pattern.compile("\\s+"))
+        }
+
+        private fun <K, V> reverseMap(map: Map<K, Set<V>>): HashMap<V, K> {
+            val reverseMap = HashMap<V, K>()
+            map.forEach { (key, setOfValues) ->
+                setOfValues.forEach { value -> reverseMap[value] = key }
+            }
+            return reverseMap
+        }
+    }
+}
